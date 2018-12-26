e129ee7d25f86b58b27a298caa1c0e5a848f02aa
Mike Kreiser <mikepkreiser@gmail.com>
2018-12-26 16:08:06 -0500

    Do not collect coverage from stubs

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.js',
     '!src/**/*.test.js',
+    '!src/**/*.stubs.js',
     '!**/node_modules/**'
   ],
   coverageDirectory: 'coverage',
