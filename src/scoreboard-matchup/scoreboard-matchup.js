f54cd8c15dc9d113014119e09c7d92d653b3ad09
Mike Kreiser <mikepkreiser@gmail.com>
2018-12-26 22:35:17 -0500

    Add ScoreboardMatchup model

---
+++ src/scoreboard-matchup/scoreboard-matchup.js
@@ -0,0 +1,72 @@
+import _ from 'lodash';
+
+import ApiModel from '../api-model/api-model.js';
+import Team from '../team/team.js';
+
+class ScoreboardMatchup extends ApiModel {
+  /**
+   * @typedef {object} ScoreboardMatchupModel
+   * @property {Team} homeTeam An instance of the home team. Uses a cached instance if possible.
+   * @property {Team} awayTeam An instance of the home team. Uses a cached instance if possible.
+   * @property {number} homeTeamScore Score of the home team.
+   * @property {number} awayTeamScore Score of the away team.
+   * @property {boolean} isByeWeek Whether or not the matchup is in a bye week.
+   * @property {Team} wunner The Team instance of the winning team.
+   */
+  static responseMap = {
+    homeTeam: {
+      key: 'teams',
+      manualParse: (responseData) => {
+        const homeTeamData = _.find(responseData, { home: true });
+        if (!homeTeamData) {
+          return undefined;
+        }
+
+        const cachedTeam = Team.get(homeTeamData.teamId);
+        return cachedTeam || Team.buildFromServer(homeTeamData.team);
+      }
+    },
+    awayTeam: {
+      key: 'teams',
+      manualParse: (responseData) => {
+        const awayTeamData = _.find(responseData, { home: false });
+        if (!awayTeamData) {
+          return undefined;
+        }
+
+        const cachedTeam = Team.get(awayTeamData.teamId);
+        return cachedTeam || Team.buildFromServer(awayTeamData.team);
+      }
+    },
+
+    homeTeamScore: {
+      key: 'teams',
+      manualParse: (responseData) => {
+        const homeTeamData = _.find(responseData, { home: true });
+        return _.get(homeTeamData, 'score');
+      }
+    },
+    awayTeamScore: {
+      key: 'teams',
+      manualParse: (responseData) => {
+        const awayTeamData = _.find(responseData, { home: false });
+        return _.get(awayTeamData, 'score');
+      }
+    },
+
+    isByeWeek: 'bye',
+    winner: {
+      key: 'teams',
+      defer: true,
+      manualParse: (responseData, response, model) => {
+        if (_.isEmpty(response.winner)) {
+          return undefined;
+        }
+
+        return response.winner === 'home' ? model.homeTeam : model.awayTeam;
+      }
+    }
+  }
+}
+
+export default ScoreboardMatchup;
---
+++ src/scoreboard-matchup/scoreboard-matchup.test.js
@@ -0,0 +1,198 @@
+import _ from 'lodash';
+
+import ApiModel from '../api-model/api-model.js';
+import Team from '../team/team.js';
+import ScoreboardMatchup from './scoreboard-matchup.js';
+
+import { localObject, serverResponse } from './scoreboard-matchup.stubs.js';
+
+describe('ScoreboardMatchup', () => {
+  let matchup;
+
+  beforeEach(() => {
+    matchup = new ScoreboardMatchup();
+  });
+
+  afterEach(() => {
+    matchup = null;
+  });
+
+  test('extends ApiModel', () => {
+    expect(matchup).toBeInstanceOf(ApiModel);
+  });
+
+  describe('attribute population from server response', () => {
+    beforeEach(() => {
+      matchup = ScoreboardMatchup.buildFromServer(serverResponse);
+    });
+
+    test('parses data correctly', () => {
+      expect(matchup).toMatchSnapshot();
+    });
+  });
+
+  describe('attribute population from local object', () => {
+    beforeEach(() => {
+      matchup = ScoreboardMatchup.buildFromLocal(localObject);
+    });
+
+    test('parses data correctly', () => {
+      expect(matchup).toMatchSnapshot();
+    });
+  });
+
+  describe('responseMap', () => {
+    const testTeamBehavior = ({ isTeamHome }) => {
+      const teamPrefix = isTeamHome ? 'home' : 'away';
+
+      describe('when the team data does not exist on the response', () => {
+        test('returns undefined', () => {
+          const data = [];
+          const returnedTeam = _.invoke(
+            ScoreboardMatchup.responseMap, `${teamPrefix}Team.manualParse`, data
+          );
+          expect(returnedTeam).toBeUndefined();
+        });
+      });
+
+      describe('when the team data does exist on the response', () => {
+        describe('when a matching team is in the cache', () => {
+          test('returns cached team', () => {
+            const teamId = 5;
+            const data = [{
+              teamId,
+              team: { teamId },
+              home: isTeamHome
+            }];
+            const cachedTeam = Team.buildFromServer({ teamId });
+
+            const returnedTeam = _.invoke(
+              ScoreboardMatchup.responseMap, `${teamPrefix}Team.manualParse`, data
+            );
+            expect(returnedTeam).toBe(cachedTeam);
+
+            Team.clearCache();
+          });
+        });
+
+        describe('when a matching team is not in the cache', () => {
+          test('returns cached team', () => {
+            const teamId = 5;
+            const data = [{
+              teamId,
+              team: { teamId },
+              home: isTeamHome
+            }];
+            jest.spyOn(Team, 'buildFromServer');
+
+            const returnedTeam = _.invoke(
+              ScoreboardMatchup.responseMap, `${teamPrefix}Team.manualParse`, data
+            );
+            expect(returnedTeam).toBeInstanceOf(Team);
+            expect(returnedTeam.teamId).toBe(teamId);
+            expect(Team.buildFromServer).toBeCalledWith(_.first(data).team);
+
+            Team.clearCache();
+          });
+        });
+      });
+    };
+
+    describe('homeTeam', () => {
+      describe('manualParse', () => {
+        testTeamBehavior({ isTeamHome: true });
+      });
+    });
+
+    describe('awayTeam', () => {
+      describe('manualParse', () => {
+        testTeamBehavior({ isTeamHome: false });
+      });
+    });
+
+    const testTeamScoreBehavior = ({ isTeamHome }) => {
+      const teamPrefix = isTeamHome ? 'home' : 'away';
+
+      describe('when there is matching team in the response data', () => {
+        test('returns the team\'s score', () => {
+          const score = 213.43;
+          const data = [{
+            score,
+            home: isTeamHome
+          }];
+          const returnedScore = _.invoke(
+            ScoreboardMatchup.responseMap, `${teamPrefix}TeamScore.manualParse`, data
+          );
+          expect(returnedScore).toBe(score);
+        });
+      });
+
+      describe('when there is not matching team in the response data', () => {
+        test('returns undefined', () => {
+          const data = [];
+          const returnedScore = _.invoke(
+            ScoreboardMatchup.responseMap, `${teamPrefix}TeamScore.manualParse`, data
+          );
+          expect(returnedScore).toBeUndefined();
+        });
+      });
+    };
+
+    describe('homeTeamScore', () => {
+      describe('manualParse', () => {
+        testTeamScoreBehavior({ isTeamHome: true });
+      });
+    });
+
+    describe('awayTeamScore', () => {
+      describe('manualParse', () => {
+        testTeamScoreBehavior({ isTeamHome: false });
+      });
+    });
+
+    describe('winner', () => {
+      describe('when the winner is the home team', () => {
+        test('returns the parsed homeTeam', () => {
+          const response = { winner: 'home' };
+          const homeTeam = new Team();
+          const model = { homeTeam };
+
+          const returnedWinner = ScoreboardMatchup.responseMap.winner.manualParse(
+            {}, response, model
+          );
+
+          expect(returnedWinner).toBe(homeTeam);
+        });
+      });
+
+      describe('when the winner is the away team', () => {
+        test('returns the parsed awayTeam', () => {
+          const response = { winner: 'away' };
+          const awayTeam = new Team();
+          const model = { awayTeam };
+
+          const returnedWinner = ScoreboardMatchup.responseMap.winner.manualParse(
+            {}, response, model
+          );
+
+          expect(returnedWinner).toBe(awayTeam);
+        });
+      });
+
+      describe('when there is no winner', () => {
+        test('returns undefined', () => {
+          const response = { winner: '' };
+          const homeTeam = new Team();
+          const awayTeam = new Team();
+          const model = { homeTeam, awayTeam };
+
+          const returnedWinner = ScoreboardMatchup.responseMap.winner.manualParse(
+            {}, response, model
+          );
+
+          expect(returnedWinner).toBeUndefined();
+        });
+      });
+    });
+  });
+});
