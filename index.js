c4787535973b3d3946064c23bf25beb04cab83f6
Mike Kreiser <mikepkreiser@gmail.com>
2018-12-28 15:56:46 -0600

    Export new classes

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ import axios from 'axios';
 import ApiModel from './src/api-model/api-model.js';
 
 import League from './src/league/league.js';
+import NFLTeam from './src/nfl-team/nfl-team.js';
+import Player from './src/player/player.js';
+import Scoreboard from './src/scoreboard/scoreboard.js';
+import ScoreboardMatchup from './src/scoreboard-matchup/scoreboard-matchup.js';
 import Team from './src/team/team.js';
+
+import {
+  nflTeamIdToNFLTeam,
+  nflTeamIdToNFLTeamAbbreviation,
+  slotCategoryIdToPositionMap
+} from './src/constants.js';
 
 axios.defaults.baseURL = 'http://games.espn.com/ffl/api/v2/';
 
@@ -11,5 +21,13 @@ export {
   ApiModel,
 
   League,
-  Team
+  NFLTeam,
+  Player,
+  Scoreboard,
+  ScoreboardMatchup,
+  Team,
+
+  nflTeamIdToNFLTeam,
+  nflTeamIdToNFLTeamAbbreviation,
+  slotCategoryIdToPositionMap
 };
