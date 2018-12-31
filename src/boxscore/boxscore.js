a2933b0188adace03aa22da6faa735d460055fca
Mike Kreiser <mikepkreiser@gmail.com>
2018-12-30 22:28:45 -0600

    Add Boxscore

---
+++ src/boxscore/boxscore.js
@@ -0,0 +1,115 @@
+import _ from 'lodash';
+
+import ApiModel from '../api-model/api-model.js';
+
+import BoxscoreTeam from '../boxscore-team/boxscore-team.js';
+import NFLGame from '../nfl-game/nfl-game.js';
+
+/**
+ * Represents the boxscore of a matchup between two teams. Requires `leagueId`, `seasonId`,
+ * `teamId`, and at least one of `matchupPeriodId` and `scoringPeriodId`.
+ *
+ * Only introduced in 2017 and valid for all matchups in 2017 or after.
+ * @extends ApiModel
+ */
+class Boxscore extends ApiModel {
+  constructor(options = {}) {
+    super(options);
+
+    /**
+     * Id of the League to which the boxscore belongs. Required to make reads on the Boxscore
+     * instance.
+     * @type {number}
+     */
+    this.leagueId = options.leagueId;
+
+    /**
+     * Id of the season (i.e. the year) to which the boxscore belongs. Required to make reads on
+     * the Boxscore instance.
+     * @type {number}
+     */
+    this.seasonId = options.seasonId;
+
+    /**
+     * Id of the team to which the boxscore includes. The team is not guaranteed to be the home or
+     * away team, but rather just guaranteed to be on the boxscore. Required to make reads on the
+     * Boxscore instance.
+     * @type {number}
+     */
+    this.teamId = options.teamId;
+
+    /**
+     * Id of the matchup period to which the boxscore refers to. This or `scoringPeriodId` must be
+     * defined to make valid read requests.
+     * @type {number}
+     */
+    this.matchupPeriodId = options.matchupPeriodId;
+
+    /**
+     * Id of the scoring period to which the boxscore refers to. This or `matchupPeriodId` must be
+     * defined to make valid read requests.
+     * @type {number}
+     */
+    this.scoringPeriodId = options.scoringPeriodId;
+  }
+
+  static displayName = 'Boxscore';
+
+  static route = 'boxscore';
+
+  /**
+   * @typedef {object} BoxscoreModel
+   *
+   * @property {number} matchupPeriodId
+   * @property {number} scoringPeriodId
+   *
+   * @property {NFLGame[]} nflGames An array of the NFL games occuring during the `scoringPeriod`.
+   *
+   * @property {BoxscoreTeam} homeTeam A BoxscoreTeam representing the home team in the matchup.
+   * @property {BoxscoreTeam} awayTeam A BoxscoreTeam representing the away team in the matchup.
+   */
+
+  /**
+    * @type {BoxscoreModel}
+    */
+  static responseMap = {
+    matchupPeriodId: 'boxscore.matchupPeriodId',
+    scoringPeriodId: 'boxscore.scoringPeriodId',
+
+    nflGames: {
+      key: 'boxscore.progames',
+      manualParse: (responseData) => _.map(responseData, (value) => NFLGame.buildFromServer(value))
+    },
+
+    homeTeam: {
+      key: 'teams',
+      manualParse: (responseData, response) => {
+        const scheduleItems = _.get(response, 'boxscore.scheduleItems');
+        const matchups = _.get(_.first(scheduleItems), 'matchups');
+        const matchup = _.first(matchups);
+        const matchupScore = _.sum(_.get(matchup, 'homeTeamScores'));
+
+        const teamId = _.get(matchup, 'homeTeamId');
+        const teamBoxscore = _.find(_.get(response, 'boxscore.teams'), { teamId });
+
+        return BoxscoreTeam.buildFromServer({ matchupScore, teamBoxscore }, { isHome: true });
+      }
+    },
+    awayTeam: {
+      key: 'teams',
+      manualParse: (responseData, response) => {
+        const scheduleItems = _.get(response, 'boxscore.scheduleItems');
+        const matchups = _.get(_.first(scheduleItems), 'matchups');
+        const matchup = _.first(matchups);
+        const matchupScore = _.sum(_.get(matchup, 'awayTeamScores'));
+
+        const teamId = _.get(matchup, 'awayTeamId');
+        const teamBoxscore = _.find(_.get(response, 'boxscore.teams'), { teamId });
+
+        return BoxscoreTeam.buildFromServer({ matchupScore, teamBoxscore }, { isHome: false });
+      }
+    }
+  };
+}
+
+export default Boxscore;
---
+++ src/boxscore/boxscore.test.js
@@ -0,0 +1,150 @@
+import _ from 'lodash';
+
+import ApiModel from '../api-model/api-model.js';
+import BoxscoreTeam from '../boxscore-team/boxscore-team.js';
+import NFLGame from '../nfl-game/nfl-game.js';
+
+import Boxscore from './boxscore.js';
+
+import { localObject, serverResponse } from './boxscore.stubs.js';
+
+describe('Boxscore', () => {
+  let boxscore;
+
+  beforeEach(() => {
+    boxscore = new Boxscore();
+  });
+
+  afterEach(() => {
+    boxscore = null;
+  });
+
+  test('extends ApiModel', () => {
+    expect(boxscore).toBeInstanceOf(ApiModel);
+  });
+
+  describe('attribute population from server response', () => {
+    beforeEach(() => {
+      boxscore = Boxscore.buildFromServer(serverResponse);
+    });
+
+    test('parses data correctly', () => {
+      expect(boxscore).toMatchSnapshot();
+    });
+  });
+
+  describe('attribute population from local object', () => {
+    beforeEach(() => {
+      boxscore = Boxscore.buildFromLocal(localObject);
+    });
+
+    test('parses data correctly', () => {
+      expect(boxscore).toMatchSnapshot();
+    });
+  });
+
+  describe('constructor', () => {
+    describe('when options are not passed', () => {
+      const testPropIsUndefined = (prop) => {
+        test(`${prop} is undefined`, () => {
+          const newBoxscore = new Boxscore();
+          expect(_.get(newBoxscore, prop)).toBeUndefined();
+        });
+      };
+
+      testPropIsUndefined('leagueId');
+      testPropIsUndefined('seasonId');
+      testPropIsUndefined('teamId');
+      testPropIsUndefined('matchupPeriodId');
+      testPropIsUndefined('scoringPeriodId');
+    });
+
+    describe('when options are passed', () => {
+      const testPropIsSetFromOptions = (prop) => {
+        test(`${prop} is set from options`, () => {
+          const value = 25;
+          const newBoxscore = new Boxscore({ [prop]: value });
+          expect(_.get(newBoxscore, prop)).toBe(value);
+        });
+      };
+
+      testPropIsSetFromOptions('leagueId');
+      testPropIsSetFromOptions('seasonId');
+      testPropIsSetFromOptions('teamId');
+      testPropIsSetFromOptions('matchupPeriodId');
+      testPropIsSetFromOptions('scoringPeriodId');
+    });
+  });
+
+  describe('responseMap', () => {
+    describe('nflGames', () => {
+      describe('manualParse', () => {
+        test('returns an array of NFLGames', () => {
+          const data = [{
+            gameId: 234
+          }, {
+            gameId: 432
+          }];
+
+          const returnedTeams = Boxscore.responseMap.nflGames.manualParse(data);
+
+          expect.hasAssertions();
+          _.forEach(returnedTeams, (team, index) => {
+            expect(team).toBeInstanceOf(NFLGame);
+            expect(team.gameId).toBe(data[index].gameId);
+          });
+        });
+      });
+    });
+
+    const testTeamBehavior = ({ prefix }) => {
+      describe('when no response is passed', () => {
+        test('returns a sparse BoxscoreTeam', () => {
+          const returnedTeam = _.invoke(Boxscore.responseMap, `${prefix}Team.manualParse`);
+          expect(returnedTeam).toEqual(BoxscoreTeam.buildFromServer({ matchupScore: 0 }));
+        });
+      });
+
+      describe('when response is passed', () => {
+        test('returns a populated BoxscoreTeam', () => {
+          const teamId = 12;
+          const scores = [100, 100];
+          const team = { teamId };
+
+          const response = {
+            boxscore: {
+              scheduleItems: [{
+                matchups: [{
+                  [`${prefix}TeamScores`]: scores,
+                  [`${prefix}TeamId`]: teamId
+                }]
+              }],
+              teams: [{}, team]
+            }
+          };
+
+          const returnedTeam = _.invoke(
+            Boxscore.responseMap, `${prefix}Team.manualParse`, undefined, response
+          );
+          const expectedTeam = BoxscoreTeam.buildFromServer({
+            matchupScore: _.sum(scores),
+            teamBoxscore: team
+          });
+          expect(returnedTeam).toEqual(expectedTeam);
+        });
+      });
+    };
+
+    describe('homeTeam', () => {
+      describe('manualParse', () => {
+        testTeamBehavior({ prefix: 'home' });
+      });
+    });
+
+    describe('awayTeam', () => {
+      describe('manualParse', () => {
+        testTeamBehavior({ prefix: 'away' });
+      });
+    });
+  });
+});
