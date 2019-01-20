ae8ff1b84ee15b9d07d51ea99816f9f71ce5b612
Mike Kreiser <mikepkreiser@gmail.com>
2019-01-20 12:03:55 -0600

    BoxscorePlayer: Cleanup tests

--- src/boxscore-player/boxscore-player.test.js
+++ src/boxscore-player/boxscore-player.test.js
@@ -5,37 +5,22 @@ import BoxscorePlayer from './boxscore-player.js';
 import { localObject, serverResponse } from './boxscore-player.stubs.js';
 
 describe('BoxscorePlayer', () => {
-  let boxscorePlayer;
-
-  beforeEach(() => {
-    boxscorePlayer = new BoxscorePlayer();
-  });
-
-  afterEach(() => {
-    boxscorePlayer = null;
-  });
-
   test('extends SlottedPlayer', () => {
-    expect(boxscorePlayer).toBeInstanceOf(SlottedPlayer);
+    const instance = new BoxscorePlayer();
+    expect(instance).toBeInstanceOf(SlottedPlayer);
   });
 
-  describe('attribute population from server response', () => {
-    beforeEach(() => {
-      boxscorePlayer = BoxscorePlayer.buildFromServer(serverResponse);
-    });
-
-    test('parses data correctly', () => {
-      expect(boxscorePlayer).toMatchSnapshot();
+  describe('when creating a team from a server response', () => {
+    test('parses and assigns data correctly', () => {
+      const instance = BoxscorePlayer.buildFromServer(serverResponse);
+      expect(instance).toMatchSnapshot();
     });
   });
 
-  describe('attribute population from local object', () => {
-    beforeEach(() => {
-      boxscorePlayer = new BoxscorePlayer(localObject);
-    });
-
-    test('parses data correctly', () => {
-      expect(boxscorePlayer).toMatchSnapshot();
+  describe('when creating a team locally', () => {
+    test('parses and assigns data correctly', () => {
+      const instance = new BoxscorePlayer(localObject);
+      expect(instance).toMatchSnapshot();
     });
   });
 });
