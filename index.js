17b94695242134bbf84690b804ccef86e423d470
Mike Kreiser <mikepkreiser@gmail.com>
2019-05-12 15:21:58 -0500

    Remove params and refactor route handling

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import axios from 'axios';
-
 import BaseObject from './src/base-object/base-object.js';
 import BaseCacheableObject from './src/base-cacheable-object/base-cacheable-object.js';
 import BaseAPIObject from './src/base-api-object/base-api-object.js';
-
-axios.defaults.baseURL = 'http://fantasy.espn.com/apis/v3/games/ffl/';
 
 export {
   BaseObject,
