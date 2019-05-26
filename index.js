8c2c581c05849336cb52c5be0230d20fd2772358
Mike Kreiser <mikepkreiser@gmail.com>
2019-05-26 14:44:52 -0500

    Organize base classes in project structure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import BaseObject from './src/base-object/base-object.js';
-import BaseCacheableObject from './src/base-cacheable-object/base-cacheable-object.js';
-import BaseAPIObject from './src/base-api-object/base-api-object.js';
+import BaseObject from './src/base-classes/base-object/base-object.js';
+import BaseCacheableObject from './src/base-classes/base-cacheable-object/base-cacheable-object.js';
+import BaseAPIObject from './src/base-classes/base-api-object/base-api-object.js';
 
 export {
   BaseObject,
