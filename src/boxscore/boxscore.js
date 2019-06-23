7e57d1e498895879296e6316b6d1ea85aed5a8cb
Mike Kreiser <mikepkreiser@gmail.com>
2019-06-23 18:22:15 -0500

    Add new Boxscore model

---
+++ src/boxscore/boxscore.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+
+import BaseObject from '../base-classes/base-object/base-object';
+
+import BoxscorePlayer from '../boxscore-player/boxscore-player';
+
+/**
+ * Represents a boxscore for a week.
+ *
+ * @extends {BaseObject}
+ */
+class Boxscore extends BaseObject {
+  static displayName = 'Boxscore';
+
+  /**
+   * @typedef {object} Boxscore~BoxscoreMap
+   *
+   * @property {number} homeScore The total points scored by the home team.
+   * @property {number} homeTeamId The home team's id. Can be used to load a cached Team.
+   * @property {BoxscorePlayer[]} homeRoster The home team's roster, containing player info and
+   *                                         stats.
+   *
+   * @property {number} awayScore The total points scored by the away team.
+   * @property {number} awayTeamId The away team's id. Can be used to load a cached Team.
+   * @property {BoxscorePlayer[]} awayRoster The away team's roster, containing player info and
+   *                                         stats.
+   */
+
+  /**
+   * @type {Boxscore~BoxscoreMap}
+   */
+  static responseMap = {
+    homeScore: 'home.totalPoints',
+    homeTeamId: 'home.teamId',
+    homeRoster: {
+      key: 'home.rosterForCurrentScoringPeriod',
+      isArray: true,
+      manualParse: (responseData, data, constructorParams) => _.map(
+        responseData,
+        (playerData) => BoxscorePlayer.buildFromServer(playerData, constructorParams)
+      )
+    },
+
+    awayScore: 'away.totalPoints',
+    awayTeamId: 'away.teamId',
+    awayRoster: {
+      key: 'away.rosterForCurrentScoringPeriod',
+      isArray: true,
+      manualParse: (responseData, data, constructorParams) => _.map(
+        responseData,
+        (playerData) => BoxscorePlayer.buildFromServer(playerData, constructorParams)
+      )
+    }
+  };
+}
+
+export default Boxscore;
---
+++ src/boxscore/boxscore.test.js
@@ -0,0 +1,78 @@
+import _ from 'lodash';
+
+import BaseObject from '../base-classes/base-object/base-object.js';
+
+import BoxscorePlayer from '../boxscore-player/boxscore-player';
+
+import Boxscore from './boxscore';
+
+describe('Boxscore', () => {
+  test('extends BaseObject', () => {
+    const instance = new Boxscore();
+    expect(instance).toBeInstanceOf(BaseObject);
+  });
+
+  describe('responseMap', () => {
+    const buildBoxscore = (data, options) => Boxscore.buildFromServer(data, options);
+
+    let data;
+    let playerData;
+
+    beforeEach(() => {
+      playerData = {
+        lineupSlotId: 2,
+        playerPoolEntry: {
+          player: {
+            stats: [{
+              appliedStats: {
+                24: 2.3,
+                25: 6
+              },
+              statSourceId: 0,
+              statSplitTypeId: 1
+            }]
+          }
+        }
+      };
+
+      data = {
+        home: {
+          totalPoints: 123,
+          teamId: 3,
+          rosterForCurrentScoringPeriod: [playerData]
+        },
+        away: {
+          totalPoints: 324,
+          teamId: 2,
+          rosterForCurrentScoringPeriod: [playerData]
+        }
+      };
+    });
+
+    describe('homeRoster', () => {
+      describe('manualParse', () => {
+        test('maps to BoxscorePlayer instances', () => {
+          const boxscore = buildBoxscore(data);
+
+          expect.hasAssertions();
+          _.forEach(boxscore.homeRoster, (player) => {
+            expect(player).toBeInstanceOf(BoxscorePlayer);
+          });
+        });
+      });
+    });
+
+    describe('awayRoster', () => {
+      describe('manualParse', () => {
+        test('maps to BoxscorePlayer instances', () => {
+          const boxscore = buildBoxscore(data);
+
+          expect.hasAssertions();
+          _.forEach(boxscore.awayRoster, (player) => {
+            expect(player).toBeInstanceOf(BoxscorePlayer);
+          });
+        });
+      });
+    });
+  });
+});
