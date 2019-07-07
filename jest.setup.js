178bc7d24b3ac41bc4310349813bf37e6d29c326
Mike Kreiser <mikepkreiser@gmail.com>
2019-07-07 12:46:10 -0500

    Refactor build outputs

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-// import { League, Roster, Team } from './index.js';
-
-// jest.setTimeout(15000);
-
-// beforeEach(() => {
-//   League.clearCache();
-//   Roster.clearCache();
-//   Team.clearCache();
-// });
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Boxscore from './boxscore/boxscore';
+import BoxscorePlayer from './boxscore-player/boxscore-player';
+import Client from './client/client';
+import FreeAgentPlayer from './free-agent-player/free-agent-player';
+import Player from './player/player';
+import PlayerStats from './player-stats/player-stats';
+import Team from './team/team';
+
+export {
+  Boxscore,
+  BoxscorePlayer,
+  Client,
+  FreeAgentPlayer,
+  Player,
+  PlayerStats,
+  Team
+};
