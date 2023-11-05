204df22f83d861a22545c1a545a16fb5d51a8f1b
Mike Kreiser <mikepkreiser@gmail.com>
2023-11-05 12:53:54 -0500

    Update babel configs to support static getter/setter

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   presets: ['@babel/preset-env'],
-  plugins: ['@babel/plugin-proposal-class-properties', 'lodash'],
+  plugins: [
+    ["@babel/plugin-transform-class-properties", { "loose": true }],
+    ["@babel/plugin-transform-private-methods", { "loose": true }],
+    ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
+    'lodash'
+  ],
   env: {
     test: {
       plugins: ['@babel/plugin-transform-runtime']
