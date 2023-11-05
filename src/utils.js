c8abd4b6ac3dfdf85255319e069943e848d68e9f
Mike Kreiser <mikepkreiser@gmail.com>
2023-11-05 10:07:17 -0500

    Refactor object flattening to prevent numerical key collisons in objects

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,35 @@
 import _ from 'lodash';
 
+const setWithWarning = (objValue, newValue, key, object) => {
+  if (object[key]) {
+    console.warn(`espn-fantasy-football-api: Duplicate key ${key} in flatten response!`);
+  }
+
+  _.set(object, key, newValue);
+};
+
 const flattenObject = (object) => {
   const flatObject = {};
 
   _.forEach(object, (value, key) => {
     if (_.isPlainObject(value)) {
-      _.assign(flatObject, flattenObject(value));
+      _.assignWith(flatObject, flattenObject(value), setWithWarning);
     } else {
-      _.set(flatObject, key, value);
+      setWithWarning(flatObject, key, value);
+    }
+  });
+
+  return flatObject;
+};
+
+const flattenObjectSansNumericKeys = (object) => {
+  const flatObject = {};
+
+  _.forEach(object, (value, key) => {
+    if (_.isPlainObject(value) && !_.some(_.keys(value), (k) => !_.isNaN(Number(k)))) {
+      _.assignWith(flatObject, flattenObjectSansNumericKeys(value), setWithWarning);
+    } else {
+      setWithWarning(flatObject, key, value);
     }
   });
 
@@ -15,5 +37,6 @@ const flattenObject = (object) => {
 };
 
 export {
-  flattenObject
+  flattenObject,
+  flattenObjectSansNumericKeys
 };
